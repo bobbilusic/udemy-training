4239bb7cb1cc66cc79ef2e338d26b3824adb31df
bobbilusic <bobbilusic@gmail.com>
2018-05-04 18:30:22 -0700

    Modified HelloWorld app.

---
+++ Udemy/src/Section4/HelloWorld.java
@@ -0,0 +1,12 @@
+package Section4;
+
+public class HelloWorld {
+
+	// Main Method
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I'm adding code to my project.");
+
+	}
+
+}
